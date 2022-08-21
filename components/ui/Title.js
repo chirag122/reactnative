d0a7edce5be8a9531e8960d2dbfa8889a8e69768
chirag122 <49075536+chirag122@users.noreply.github.com>
2022-08-21 12:11:53 +0530

    v2

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -11,11 +11,13 @@ const styles = StyleSheet.create({
     title: {
         fontFamily: 'open-sans-bold',
         fontSize: 18,
-        fontWeight: 'bold',
-        color: Colors.accent500,
+        // fontWeight: 'bold',
+        color: 'white',
         textAlign: 'center',
         borderWidth: 2,
-        borderColor: Colors.accent500,
+        borderColor: 'white',
         padding: 12,
+        maxWidth: '80%',
+        width: 300,
     }
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,6 +3,9 @@ import {
     View,
     StyleSheet,
     Alert,
+    useWindowDimensions,
+    KeyboardAvoidingView,
+    ScrollView,
 } from 'react-native';
 import { useState } from 'react';
 import PrimaryButton from '../components/ui/PrimaryButton';
@@ -13,6 +16,8 @@ import InstructionText from '../components/ui/InstructionText';
 
 function StartGameScreen({ onPickNumber }) {
     const [enteredNumber, setEnteredNumber] = useState('');
+
+    const { width, height } = useWindowDimensions();
 
     function numberInputHandler(enteredText) {
         setEnteredNumber(enteredText)
@@ -35,39 +40,50 @@ function StartGameScreen({ onPickNumber }) {
         onPickNumber(chosenNumber);
     }
 
+    const marginTopDistance = height < 580 ? 50 : 100;
+
     return (
-        <View style={styles.rootContainer}>
-            <Title>Guess my Number</Title>
-            <Card>
-                <InstructionText>Enter a number</InstructionText>
-                <TextInput
-                    style={styles.numberInput}
-                    maxLength={2}
-                    keyboardType={'number-pad'}
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    onChangeText={numberInputHandler}
-                    value={enteredNumber}
-                />
-                <View style={styles.buttonsContainer}>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-                    </View>
+        <ScrollView style={styles.screen}>
+            <KeyboardAvoidingView style={styles.screen} behavior="position">
+                <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
+                    <Title>Guess my Number</Title>
+                    <Card>
+                        <InstructionText>Enter a number</InstructionText>
+                        <TextInput
+                            style={styles.numberInput}
+                            maxLength={2}
+                            keyboardType={'number-pad'}
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                            onChangeText={numberInputHandler}
+                            value={enteredNumber}
+                        />
+                        <View style={styles.buttonsContainer}>
+                            <View style={styles.buttonContainer}>
+                                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                            </View>
+                            <View style={styles.buttonContainer}>
+                                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                            </View>
+                        </View>
+                    </Card>
                 </View>
-            </Card>
-        </View>
+            </KeyboardAvoidingView>
+        </ScrollView>
     );
 }
 
 export default StartGameScreen;
 
+// const deviceHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+    },
     rootContainer: {
         flex: 1,
-        marginTop: 100,
+        // marginTop: deviceHeight < 400 ? 50 : 100,
         alignItems: 'center',
     },
     instructionText: {
